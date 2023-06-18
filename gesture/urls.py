c2fe75f53972afbfe745d05fd42ed97dce9f8996
Prem Ghosh <prem.219310162@muj.manipal.edu>
2023-06-19 03:23:14 +0530

    pff

--- gesture/urls.py
+++ gesture/urls.py
@@ -13,16 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
 
+from django.urls import path, re_path, include
 from django.views.static import serve
-from django.conf.urls import url
+from django.conf import settings
+from django.contrib import admin
 
 urlpatterns = [
-    path('',include('findproj.urls')),
+    path('', include('findproj.urls')),
     path('admin/', admin.site.urls),
-    
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
